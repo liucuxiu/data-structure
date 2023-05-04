46a3d6b0e3a73459e85c833581d482b537afde3a
liucuxiu <xliucuxiux@gmail.com>
2023-05-04 10:30:39 +0700

    83. Remove Duplicates from Sorted List

---
+++ linkedlist/excersise/removeDuplicate.js
@@ -0,0 +1,24 @@
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val);
+  this.next = (next === undefined ? null : next);
+}
+
+function removeDuplicate(head) {
+  let currentNode = head
+
+  while (currentNode && currentNode.next) {
+    if (currentNode.val === currentNode.next.val) {
+      //remove
+      currentNode.next = currentNode.next.next
+      console.log(currentNode)
+    }
+    else {
+      currentNode = currentNode.next
+    }
+  }
+
+  return head
+}
+// 1  1  1
+const l1 = new ListNode(1, new ListNode(1, new ListNode(1)));
+console.log(removeDuplicate(l1))
